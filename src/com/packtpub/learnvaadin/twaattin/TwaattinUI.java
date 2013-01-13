d07750f0d4db8b8c6b7c0d1473009a22a7056358
Nicolas Frankel <nicolas@frankel.ch>
2013-01-13 18:11:06 +0100

    Added serialVersionUID to UI

--- src/com/packtpub/learnvaadin/twaattin/TwaattinUI.java
+++ src/com/packtpub/learnvaadin/twaattin/TwaattinUI.java
@@ -1,13 +1,16 @@
 package com.packtpub.learnvaadin.twaattin;
 
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.*;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
 
 public class TwaattinUI extends UI {
+
+	private static final long serialVersionUID = 1L;
+	
 	@Override
 	public void init(VaadinRequest request) {
 		Label label = new Label("Hello Vaadin user");
 		setContent(label);
 	}
-
 }
