fcfd0b6e5bcfe8848461f792ad870d8c8ee8f26c
Nicolas Frankel <nicolas@frankel.ch>
2013-01-19 23:25:29 +0100

    Updated timeline screen to display user logged in as well as a logout button

--- src/com/packtpub/learnvaadin/twaattin/ui/TimelineScreen.java
+++ src/com/packtpub/learnvaadin/twaattin/ui/TimelineScreen.java
@@ -1,5 +1,14 @@
 package com.packtpub.learnvaadin.twaattin.ui;
 
+import static com.vaadin.server.Sizeable.Unit.PERCENTAGE;
+import static com.vaadin.ui.Alignment.MIDDLE_RIGHT;
+
+import java.security.Principal;
+
+import com.packtpub.learnvaadin.twaattin.presenter.LogoutBehavior;
+import com.vaadin.server.VaadinSession;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
@@ -10,6 +19,20 @@ public class TimelineScreen extends VerticalLayout {
 	public TimelineScreen() {
 
 		setMargin(true);
+
+		Label label = new Label(VaadinSession.getCurrent()
+				.getAttribute(Principal.class).getName());
+
+		Button button = new Button("Logout");
+
+		button.addClickListener(new LogoutBehavior());
+
+		HorizontalLayout menuBar = new HorizontalLayout(label, button);
+
+		menuBar.setWidth(100, PERCENTAGE);
+		menuBar.setComponentAlignment(button, MIDDLE_RIGHT);
+
+		addComponent(menuBar);
 
 		fillTweets();
 	}
