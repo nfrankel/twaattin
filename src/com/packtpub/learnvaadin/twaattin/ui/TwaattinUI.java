c10c7613a3e3e4b56754b6ac2bdb25226cdd802a
Nicolas Frankel <nicolas@frankel.ch>
2013-01-19 22:33:36 +0100

    Replaced the split panel by the login screen in the UI

--- src/com/packtpub/learnvaadin/twaattin/ui/TwaattinUI.java
+++ src/com/packtpub/learnvaadin/twaattin/ui/TwaattinUI.java
@@ -1,9 +1,6 @@
 package com.packtpub.learnvaadin.twaattin.ui;
 
-import static com.vaadin.server.Sizeable.Unit.PIXELS;
-
 import com.vaadin.server.VaadinRequest;
-import com.vaadin.ui.HorizontalSplitPanel;
 import com.vaadin.ui.UI;
 
 public class TwaattinUI extends UI {
@@ -13,13 +10,6 @@ public class TwaattinUI extends UI {
 	@Override
 	public void init(VaadinRequest request) {
 
-		HorizontalSplitPanel panel = new HorizontalSplitPanel();
-
-		panel.setFirstComponent(new LoginScreen());
-		panel.setSecondComponent(new TimelineScreen());
-
-		panel.setSplitPosition(300, PIXELS);
-
-		setContent(panel);
+		setContent(new LoginScreen());
 	}
 }
