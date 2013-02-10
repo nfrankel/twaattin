ab1bc16d9555d1ec7a84309bb4064229cbc4e555
Nicolas Frankel <nicolas@frankel.ch>
2013-02-10 23:12:42 +0100

    Added facade over Twitter4J API

---
+++ src/com/packtpub/learnvaadin/service/TwitterService.java
@@ -0,0 +1,58 @@
+package com.packtpub.learnvaadin.service;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
+public class TwitterService {
+
+	private Twitter twitter = TwitterFactory.getSingleton();
+
+	private static TwitterService singleton = new TwitterService();
+
+	private RequestToken requestToken;
+
+	private AccessToken accessToken;
+
+	public static TwitterService get() {
+
+		return singleton;
+	}
+
+	public String getAuthenticationUrl() {
+
+		twitter.setOAuthAccessToken(null);
+
+		try {
+
+			requestToken = twitter.getOAuthRequestToken();
+
+			return requestToken.getAuthenticationURL();
+
+		} catch (TwitterException e) {
+
+			throw new RuntimeException(e);
+		}
+	}
+
+	public String authenticate(String pin) throws TwitterException {
+
+		accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+
+		requestToken = null;
+
+		twitter.setOAuthAccessToken(accessToken);
+
+		return twitter.getScreenName();
+	}
+
+	public List<Status> getTweets() throws TwitterException {
+
+		return twitter.getUserTimeline();
+	}
+}
