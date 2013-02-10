9d31f8a3865bd151170975f0a78220437cc6a6ad
Nicolas Frankel <nicolas@frankel.ch>
2013-02-10 23:15:48 +0100

    Updated login screen with components for Twitter authentication

--- src/com/packtpub/learnvaadin/twaattin/ui/LoginScreen.java
+++ src/com/packtpub/learnvaadin/twaattin/ui/LoginScreen.java
@@ -1,28 +1,37 @@
 package com.packtpub.learnvaadin.twaattin.ui;
 
+import com.packtpub.learnvaadin.service.TwitterService;
 import com.packtpub.learnvaadin.twaattin.presenter.LoginBehavior;
+import com.vaadin.server.ExternalResource;
 import com.vaadin.ui.Button;
-import com.vaadin.ui.FormLayout;
-import com.vaadin.ui.PasswordField;
+import com.vaadin.ui.Link;
 import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 
-public class LoginScreen extends FormLayout {
+public class LoginScreen extends VerticalLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	private TextField loginField = new TextField("Login", "packtpub");
-	private PasswordField passwordField = new PasswordField("Password");
+	private Link twitterLink = new Link();
+	private TextField pinField = new TextField();
 	private Button submitButton = new Button("Submit");
 
 	public LoginScreen() {
 
 		setMargin(true);
+		setSpacing(true);
 
-		addComponent(loginField);
-		addComponent(passwordField);
+		twitterLink.setCaption("Get PIN");
+		twitterLink.setTargetName("twitterauth");
+		twitterLink.setResource(new ExternalResource(TwitterService.get()
+				.getAuthenticationUrl()));
+
+		pinField.setInputPrompt("PIN");
+
+		addComponent(twitterLink);
+		addComponent(pinField);
 		addComponent(submitButton);
 
-		submitButton.addClickListener(new LoginBehavior(loginField,
-				passwordField));
+		submitButton.addClickListener(new LoginBehavior(pinField));
 	}
 }
