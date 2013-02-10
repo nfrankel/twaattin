86948dba50d382f73afc5e29d8da60e4c7d9ebfa
Nicolas Frankel <nicolas@frankel.ch>
2013-02-10 23:14:35 +0100

    Changed login behavior to use Twitter authentication strategy

--- src/com/packtpub/learnvaadin/twaattin/presenter/LoginBehavior.java
+++ src/com/packtpub/learnvaadin/twaattin/presenter/LoginBehavior.java
@@ -5,32 +5,27 @@ import static com.vaadin.ui.Notification.Type.ERROR_MESSAGE;
 import java.security.Principal;
 
 import com.packtpub.learnvaadin.authentication.AuthenticationException;
-import com.packtpub.learnvaadin.authentication.SimpleUserPasswordAuthenticationStrategy;
-import com.packtpub.learnvaadin.authentication.UserPasswordAuthenticationStrategy;
+import com.packtpub.learnvaadin.authentication.TwitterAuthenticationStrategy;
 import com.packtpub.learnvaadin.twaattin.ui.TimelineScreen;
 import com.vaadin.server.VaadinSession;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Notification;
-import com.vaadin.ui.PasswordField;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 
 /**
- * Login behavior delegates to a predefined
- * {@link UserPasswordAuthenticationStrategy}.
+ * Login behavior delegates to a predefined authentication strategy.
  */
 public class LoginBehavior implements ClickListener {
 
 	private static final long serialVersionUID = 1L;
 
-	private final TextField loginField;
-	private final PasswordField passwordField;
+	private final TextField pinField;
 
-	public LoginBehavior(TextField loginField, PasswordField passwordField) {
+	public LoginBehavior(TextField loginField) {
 
-		this.loginField = loginField;
-		this.passwordField = passwordField;
+		this.pinField = loginField;
 	}
 
 	@Override
@@ -38,11 +33,10 @@ public class LoginBehavior implements ClickListener {
 
 		try {
 
-			String login = loginField.getValue();
-			String password = passwordField.getValue();
+			String pin = pinField.getValue();
 
-			Principal user = new SimpleUserPasswordAuthenticationStrategy()
-					.authenticate(login, password);
+			Principal user = new TwitterAuthenticationStrategy()
+					.authenticate(pin);
 
 			VaadinSession.getCurrent().setAttribute(Principal.class, user);
 
